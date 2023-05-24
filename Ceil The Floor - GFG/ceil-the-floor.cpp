7847da3b8cd627f15daf46007d8268611eb67d37
AnuragBakode <121752187+AnuragBakode@users.noreply.github.com>
2023-05-24 21:55:08 +0530

    Added solution - LeetHub

---
+++ Ceil The Floor - GFG/ceil-the-floor.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+pair<int, int> getFloorAndCeil(int arr[], int n, int x);
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        auto ans = getFloorAndCeil(arr, n, x);
+        cout << ans.first << " " << ans.second << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+pair<int, int> getFloorAndCeil(int arr[], int n, int x) {
+    // code here
+    pair<int , int> ans;
+    ans.first = -1;
+    ans.second = INT_MAX;
+    
+    for(int i = 0 ; i < n ; i++){
+        if(arr[i] <= x){
+            ans.first = max(ans.first, arr[i]);
+        }
+        
+        if(arr[i] >= x){
+            ans.second = min(ans.second, arr[i]);
+        }
+        
+    }
+    
+    if(ans.second == INT_MAX){
+        ans.second = -1;
+    }
+    
+    return ans;
+}
